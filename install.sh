f0f8974821412d16dcee576287fbacf4764f685a
demetresmeliates <demetresmeliates@gmail.com>
2025-03-16 17:30:45 +0000

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -103,6 +103,11 @@ install_path() {
     else
         # It's a file
         if [[ "$dest_path" == "$DEST_SBIN" ]]; then
+
+            # Check if it is lightdm.conf and exit if it is:
+            if [[ "$source_path" == "share/sch-scripts/configs/lightdm.conf" ]]; then
+               return 0
+            fi
             install -o root -g root -m 0755 "$source_path" "$dest_path"
         elif [[ "$source_path" == *.py ]] || [[ "$source_path" == *.sh ]]; then
             install -o root -g root -m 0755 "$source_path" "$dest_path"
@@ -220,13 +225,14 @@ install_wallpapers() {
   project_root=$(pwd)
 
   for server in "${SERVER_WALLPAPERS[@]}"; do
-    if [[ "$hostname" == "$server" ]]; then
-      wallpaper_file="$server"_$(echo "$mode" | tr '[:upper:]' '[:lower:]').png
-      # Use the full path to the wallpaper file and correct variable.
-      install -o root -g root -m 0644 "$project_root/$PROJECT_BACKGROUNDS/$wallpaper_file" "$DEST_BACKGROUNDS/School-Wallpapers/$wallpaper_file" || { echo -e "\\e[1mΣφάλμα: Αποτυχία μετακίνησης των αρχείων στους προορισμούς τους.\\e[0m"; exit 1; }
-      break
-    fi
-  done
+        if [[ "$hostname" == "$server" ]]; then
+            wallpaper_file="$server"_$(echo "$mode" | tr '[:upper:]' '[:lower:]').png
+            # Use the full path to the wallpaper file.
+            install -o root -g root -m 0644 "$project_root/$PROJECT_BACKGROUNDS/$wallpaper_file" "$DEST_BACKGROUNDS/School-Wallpapers/$wallpaper_file" || { echo -e "\\e[1mΣφάλμα: Αποτυχία μετακίνησης των αρχείων στους προορισμούς τους.\\e[0m"; exit 1; }
+            break
+        fi    
+   
+    done
 }
 
 # Detect the user with id 1000 (the first normal user):
@@ -422,39 +428,39 @@ install_sch() {
         #Check if lightdm is running
         if systemctl is-active --quiet lightdm; then
             # Set the wallpaper using dconf.
-            mode=$(get_mode)
-            wallpaper_file="/usr/share/backgrounds/School-Wallpapers/${hostname}_$(echo "$mode" | tr '[:upper:]' '[:lower:]').png"
+          mode=$(get_mode)
+          wallpaper_file="/usr/share/backgrounds/School-Wallpapers/${hostname}_$(echo "$mode" | tr '[:upper:]' '[:lower:]').png"
+          #Check if lightdm.conf exists.
+            if [[ -f /etc/lightdm/lightdm.conf ]]; then
+               if [[ "$mode" == "light" ]]; then
+                   sed -i "/^\[Seat:\*\]/a background=$wallpaper_file" /etc/lightdm/lightdm.conf
+               else
+                   sed -i "/^\[Seat:\*\]/a background=$wallpaper_file" /etc/lightdm/lightdm.conf
+               fi
+               #Check if the lines exists
+               if ! grep -q "^greeter-hide-users=false" /etc/lightdm/lightdm.conf; then
+                  #add greeter-hide-users=false
+                    sed -i "/^\[Seat:\*\]/a greeter-hide-users=false" /etc/lightdm/lightdm.conf
+               fi
+               if ! grep -q "^autologin-user=testuser" /etc/lightdm/lightdm.conf; then
+                  #Add autologin-user=testuser
+                  sed -i "/^\[Seat:\*\]/a autologin-user=testuser" /etc/lightdm/lightdm.conf
+               fi
+           else
+             echo -e "\\e[1mΣφάλμα: Το αρχείο /etc/lightdm/lightdm.conf δεν υπάρχει.\\e[0m"; exit 1;
+           fi
 
-            # Check if the wallpaper file exists
-            if [[ -f "$wallpaper_file" ]]; then
-                #Check if the config folder exist
-                if [ ! -d /etc/lightdm/lightdm-gtk-greeter.conf.d ]; then
-                    mkdir -p /etc/lightdm/lightdm-gtk-greeter.conf.d
-                fi
-                #Check if the file exist
-                if [[ -f /etc/lightdm/lightdm-gtk-greeter.conf.d/99_linuxmint.conf ]]; then
-                   #Read the file
-                    file_content=$(cat /etc/lightdm/lightdm-gtk-greeter.conf.d/99_linuxmint.conf)
-                   #Check if file has the right syntax
-                    if ! grep -q "^\[.*]" /etc/lightdm/lightdm-gtk-greeter.conf.d/99_linuxmint.conf; then
-                       echo -e "\\e[1mΣφάλμα: Το αρχείο /etc/lightdm/lightdm-gtk-greeter.conf.d/99_linuxmint.conf δεν έχει την σωστή σύνταξη.\\e[0m"; exit 1;
-                    fi
-                    #check if the line exist
-                    if echo "$file_content" | grep -q "background="; then
-                       #comment out the line background=
-                        sed -i 's/^\(background=\).*/#\1/' /etc/lightdm/lightdm-gtk-greeter.conf.d/99_linuxmint.conf
-                    fi
-                fi
-                # Write the background line to a new file:
-                echo "[greeter]" > /etc/lightdm/lightdm-gtk-greeter.conf.d/98-sch-scripts.conf
-                echo "background=$wallpaper_file" >> /etc/lightdm/lightdm-gtk-greeter.conf.d/98-sch-scripts.conf
+        fi
+        
+    else
+        echo -e "\\e[1mΗ εγκατάσταση δεν είναι στο server 1ek-volou. Εξοδος.\\e[0m"
+        return 0
 
-            fi
         fi
     fi
     #Start the service
     start_shared_folders_service
-
+    
     echo -e "\\e[1mΗ εγκατάσταση των sch-scripts ολοκληρώθηκε με επιτυχία!\\e[0m"
 }
 
