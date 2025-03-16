05729ea58b1e5ebcc08423920b9531c27b47005d
demetresmeliates <demetresmeliates@gmail.com>
2025-03-16 18:12:10 +0000

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,6 @@ set -e
 
 # --- Global Variables ---
 REVERT=false
-
 # Define variables
 DEST_ETC="/etc"
 DEST_LIB="/lib"
@@ -26,27 +25,16 @@ DEST_SHARE="/usr/share"
 DEST_SBIN="/usr/sbin"
 DEST_ROOT="/usr/share/sch-scripts"
 DEST_CONFIGS="/usr/share/sch-scripts/configs"
-DEST_UI="/usr/share/sch-scripts/ui"
-DEST_BINS="/usr/share/sch-scripts/scripts"
-
+DEST_UI="/usr/share/sch-scripts/ui"DEST_BINS="/usr/share/sch-scripts/scripts"
 PROJECT_ETC="etc"
 PROJECT_LIB="lib"
-PROJECT_SHARE="share"
+PROJECT_SHARE="share"PROJECT_SBIN="sbin"
 PROJECT_SBIN="sbin"
 PROJECT_ROOT="share/sch-scripts"
 PROJECT_CONFIGS="share/sch-scripts/configs"
 PROJECT_UI="share/sch-scripts/ui"
 PROJECT_BINS="share/sch-scripts/scripts"
 PROJECT_BACKGROUNDS="share/backgrounds/linuxmint"
-DEST_BACKGROUNDS="/usr/share/backgrounds"
-DEST_LINUXMINT_WALLPAPERS="/usr/share/backgrounds/linuxmint"
-
-# Dependencies
-DEPENDENCIES="python3 python3-gi python3-pip epoptes openssh-server iputils-arping libgtk-3-0 librsvg2-common policykit-1 util-linux dnsmasq ethtool net-tools p7zip-rar squashfs-tools symlinks"
-
-# Uninstall Dependencies
-UNINSTALL_DEPENDENCIES="epoptes openssh-server net-tools symlinks"
-
 # Configurations variables
 SHARE_DIR="/home/Shared"
 PUBLIC_DIR="/home/Shared/Public"
@@ -103,14 +91,6 @@ install_path() {
         }
     else
         # It's a file
-        if [[ "$dest_path" == "$DEST_SBIN" ]]; then
-
-            # Check if it is lightdm.conf and exit if it is:
-            if [[ "$source_path" == "share/sch-scripts/configs/lightdm.conf" ]]; then
-               return 0
-            fi
-            install -o root -g root -m 0755 "$source_path" "$dest_path"
-        elif [[ "$source_path" == *.py ]] || [[ "$source_path" == *.sh ]]; then
             install -o root -g root -m 0755 "$source_path" "$dest_path"
         else
             install -o root -g root -m 0644 "$source_path" "$dest_path"
@@ -127,6 +107,14 @@ wait_apt_lock() {
 }
 
 #install-dependencies
+
+# Dependencies
+DEPENDENCIES="python3 python3-gi python3-pip epoptes openssh-server iputils-arping libgtk-3-0 librsvg2-common policykit-1 util-linux dnsmasq ethtool net-tools p7zip-rar squashfs-tools symlinks"
+
+# Uninstall Dependencies
+UNINSTALL_DEPENDENCIES="epoptes openssh-server net-tools symlinks"
+
+
 install_dependencies() {
     wait_apt_lock
     apt-get update
@@ -187,80 +175,14 @@ configure_teachers() {
     fi
 }
 
-#Get the hostname
-get_hostname() {
-  hostname
-}
 
-#Get the theme mode
-get_mode() {
-  local current_theme
-  local dark_themes=("Adwaita-dark" "Yaru-dark" "Dark")
-  local light_themes=("Adwaita" "Yaru" "Light")
-  current_theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
-  current_theme=${current_theme//\'/}
 
-  local is_dark=false
-  for theme in "${dark_themes[@]}"; do
-    if [[ "$current_theme" == *"$theme"* ]]; then
-      is_dark=true
-      break
-    fi
-  done
-
-  if "$is_dark"; then
-    echo "dark"
-  else
-    echo "light"
-  fi
-}
-
-# Install wallpapers
-install_wallpapers() {
-  local hostname wallpaper_file mode project_root
-  SERVER_WALLPAPERS=("1ek-volou") #Add server names here
-  hostname=$(get_hostname)
-    mode=$(get_mode)
-  # Get the root directory of the project
-  project_root=$(pwd)
-
-  for server in "${SERVER_WALLPAPERS[@]}"; do
-        if [[ "$hostname" == "$server" ]]; then
-            wallpaper_file="$server"_$(echo "$mode" | tr '[:upper:]' '[:lower:]').png
-            
-            # Create the directory if it does not exist and set permissions
-            mkdir -p "$DEST_LINUXMINT_WALLPAPERS" 
-            chmod 755 "$DEST_LINUXMINT_WALLPAPERS"
-           
-             #Check if the default_background.png exists and rename it
-             if [[ -f "$DEST_LINUXMINT_WALLPAPERS/default_background.png" ]]; then
-                mv "$DEST_LINUXMINT_WALLPAPERS/default_background.png" "$DEST_LINUXMINT_WALLPAPERS/default_background.png.bak"
-            fi
-             # Copy the file
-             install -o root -g root -m 0644 "$project_root/$PROJECT_BACKGROUNDS/$wallpaper_file" "$DEST_LINUXMINT_WALLPAPERS/$wallpaper_file" || { echo -e "\\e[1mΣφάλμα: Αποτυχία μετακίνησης των αρχείων στους προορισμούς τους.\\e[0m"; exit 1; }
-              # Copy the file to default_background.png
-              cp "$DEST_LINUXMINT_WALLPAPERS/$wallpaper_file" "$DEST_LINUXMINT_WALLPAPERS/default_background.png" || { echo -e "\\e[1mΣφάλμα: Αποτυχία μετακίνησης των αρχείων στους προορισμούς τους.\\e[0m"; exit 1; }
-           
-            # Set permissions for the directory
-            chmod 755 "$DEST_LINUXMINT_WALLPAPERS" || { echo -e "\\e[1mΣφάλμα: Αδυναμία αλλαγής των δικαιωμάτων του καταλόγου $DEST_LINUXMINT_WALLPAPERS.\\e[0m"; exit 1; }
-            break
-        fi    
-   
-  done
-}
 
 # Detect the user with id 1000 (the first normal user):
 detect_administrator() {
   # Detect the administrator user
     # shellcheck disable=SC2034
     administrator="$(id -u 1000 >/dev/null && id -un 1000)"
-}
-
-#start_shared_folders service
-start_shared_folders_service() {
-    echo -e "\\e[1mΕκκίνηση shared-folders.service...\\e[0m"
-    systemctl start shared-folders.service || { echo -e "\\e[1mΣφάλμα: Αποτυχία εκκίνησης του shared-folders.service.\\e[0m"; exit 1;}
-    echo -e "\\e[1mΤο shared-folders.service ξεκίνησε με επιτυχία.\\e[0m"
 }
 
 #Create the public folder
@@ -398,14 +320,6 @@ revert_files() {
     #Revert shared-folders
     rm -rf "$DEST_SBIN/shared-folders"
     #Revert wallpaper
-
-    #Remove the lightdm changes
-
-    rm -rf /etc/lightdm/lightdm-gtk-greeter.conf.d/98-sch-scripts.conf
-    rm -rf /etc/lightdm/lightdm-gtk-greeter.conf.d/99-sch-scripts.conf
-
-
-    echo -e "\\e[1mΟι αλλαγές στα αρχεία επαναφέρθηκαν με επιτυχία.\\e[0m"
 }
 
 # Install function
@@ -420,62 +334,6 @@ install_sch() {
     create_public_folder
     #This are the configurations
     configure_teachers
-    hostname=$(get_hostname)
-    if [[ "$hostname" == "1ek-volou" ]]; then
-        #Install the wallpapers
-        install_wallpapers
-        #Configure LightDM
-          # Check if lightdm.conf exists.
-          if [[ -f /etc/lightdm/lightdm.conf ]]; then
-              # Check if greeter-session is set.
-              if ! grep -q "^greeter-session" /etc/lightdm/lightdm.conf; then
-                  # Add greeter-session=lightdm-gtk-greeter
-                  echo "greeter-session=lightdm-gtk-greeter" >> /etc/lightdm/lightdm.conf
-              else
-                  # Check if greeter-session is different than lightdm-gtk-greeter
-                  if ! grep -q "^greeter-session=lightdm-gtk-greeter" /etc/lightdm/lightdm.conf; then
-                      sed -i "s/^greeter-session=.*/greeter-session=lightdm-gtk-greeter/" /etc/lightdm/lightdm.conf
-                  fi
-              fi
-          fi
-        #Check if lightdm is running
-        if systemctl is-active --quiet lightdm; then
-          mode=$(get_mode)
-          wallpaper_file="/usr/share/backgrounds/linuxmint/${hostname}_$(echo "$mode" | tr '[:upper:]' '[:lower:]').png"
-            sed -i "/^\[Seat:\*\]/s/^\(background=\).*/\1$wallpaper_file/" /etc/lightdm/lightdm.conf || sed -i "/^\[Seat:\*\]/a background=$wallpaper_file" /etc/lightdm/lightdm.conf
-          
-            # Check if the line "background=" exists
-            if grep -q "^background=" /etc/lightdm/lightdm.conf; then
-              # Modify the existing line
-              sed -i "/^\[Seat:\*\]/s/^\(background=\).*/\1$wallpaper_file/" /etc/lightdm/lightdm.conf
-            else
-              # Add a new line
-              sed -i "/^\[Seat:\*\]/a background=$wallpaper_file" /etc/lightdm/lightdm.conf
-            fi
-
-
-                #Check if the lines exists
-                if ! grep -q "^greeter-hide-users=false" /etc/lightdm/lightdm.conf; then
-                   #add greeter-hide-users=false
-                    
-                    sed -i "/^\[Seat:\*\]/a greeter-hide-users=false" /etc/lightdm/lightdm.conf
-                fi
-                if ! grep -q "^autologin-user=testuser" /etc/lightdm/lightdm.conf; then
-                     sed -i "/^\[Seat:\*\]/a greeter-hide-users=false" /etc/lightdm/lightdm.conf
-                fi
-                if ! grep -q "^autologin-user=testuser" /etc/lightdm/lightdm.conf; then
-                   #Add autologin-user=testuser
-                   sed -i "/^\[Seat:\*\]/a autologin-user=testuser" /etc/lightdm/lightdm.conf
-                fi
-            fi
-        
-        
-    else
-        echo -e "\\e[1mΗ εγκατάσταση δεν είναι στο server 1ek-volou. Εξοδος.\\e[0m"
-        return 0
-    fi
-    #Start the service
-    start_shared_folders_service
     
     echo -e "\\e[1mΗ εγκατάσταση των sch-scripts ολοκληρώθηκε με επιτυχία!\\e[0m"
 }
@@ -497,7 +355,7 @@ remove_sch() {
 # This is the main
 main() {
 
-    if [[ "$1" == "-u" ]]; then
+        if [[ "$1" == "-u" ]]; then
         REVERT=true
     fi
     echo "Value of REVERT inside main: $REVERT"
