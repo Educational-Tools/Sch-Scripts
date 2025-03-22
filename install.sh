5f2cdb8a009907a2399fc05c283c20db2368dafd
demetresmeliates <demetresmeliates@gmail.com>
2025-03-22 10:42:00 +0000

    deleted:    etc/systemd/system/user-defaults.service 	modified:   install.sh 	deleted:    sbin/user-defaults 	deleted:    share/sch-scripts/user_defaults.py

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-    echo "This script requires root privileges. Please enter your sudo password:"
+    echo -e "\033[1mΑυτό το σενάριο πρέπει να εκκινηθεί με δικαιώματα διαχειριστή: Παρακαλώ εισάγετε τον κωδικό σας sudo:\033[1m"
     sudo true || {
-        echo "Error: Failed to obtain root privileges."
+        echo -e "\033[1mΣφάλμα: Αποτυχία λήψης δικαιωμάτων sudo.\033[1m"
         exit 1
     }
     sudo bash "$0" "$@"
@@ -48,10 +48,10 @@ SHARE_DIR="/home/Shared"
 PUBLIC_DIR="/home/Shared/Public"
 
 # Error messages.
-ERROR_INSTALL_DEPENDENCIES="Error: Failed to install dependencies."
-ERROR_MOVE_FILES="Error: Failed to move files to their destinations."
-ERROR_REVERT_FILES="Error: Failed to revert files to their original destinations."
-ERROR_REMOVE_DEPENDENCIES="Error: Failed to remove dependencies."
+ERROR_INSTALL_DEPENDENCIES="\033[1mΣφάλμα: Αποτυχία εγκατάστασης εξερτήσεων.\033[1m"
+ERROR_MOVE_FILES="\033[1mΣφάλμα: Αποτυχία μετακίνησης αρχείων στην τοποθεσία τους.\033[1m"
+ERROR_REVERT_FILES="\033[1mΣφάλμα: Αποτυχία επαναφοράς αρχείων.\033[1m"
+ERROR_REMOVE_DEPENDENCIES="\033[1mΣφάλμα: Αποτυχία απεγκατάστασης εξαρτήσεων.\033[1m"
 
 
 backup_file() {
@@ -73,7 +73,7 @@ revert_file() {
     local file_path="$dest_dir/$(basename "$source_file")"
     local bak_file="$file_path.bak"
     if [[ -f "$bak_file" ]]; then
-        echo "Restoring: $file_path from $bak_file"
+        echo -e "\033[1mΕπαναφορά: $file_path από $bak_file\033[1m"
         mv "$bak_file" "$file_path"
     else
         rm -f "$file_path"
@@ -88,7 +88,7 @@ install_path() {
         # If it's a directory, use cp -r to copy recursively.
         mkdir -p "$dest_path/$(basename "$source_path")"
         cp -r "$source_path"/* "$dest_path/$(basename "$source_path")" || { 
-          echo "Failed to copy directory."
+          echo -e "\033[1mΑποτυχία αντιγραφής καταλόγου.\033[1m"
           exit 1
         }
     else
@@ -105,7 +105,7 @@ install_path() {
 # Wait for apt lock.
 wait_apt_lock() {
     while ! flock -w 10 /var/lib/dpkg/lock-frontend -c :; do
-        echo "Waiting for apt lock to be released..."
+        echo -e "\033[1mΑναμονή για apt lock...\033[1m"
         sleep 1
     done
 }
@@ -113,13 +113,13 @@ install_dependencies() {
     wait_apt_lock
     apt-get update
     apt-get install -y -o APT::Acquire::http::Pipeline-Depth=0 -o APT::Acquire::Retries=10 $DEPENDENCIES || {
-        echo "$ERROR_INSTALL_DEPENDENCIES"
+        echo -e "$ERROR_INSTALL_DEPENDENCIES"
         exit 1 
     }
 }
 remove_dependencies() {
     apt-get remove --allow-remove-essential $UNINSTALL_DEPENDENCIES || {
-        echo "$ERROR_REMOVE_DEPENDENCIES"
+        echo -e "$ERROR_REMOVE_DEPENDENCIES"
         exit 1
     }
 }
@@ -151,99 +151,80 @@ detect_administrator() {
 }
 start_services() {   
     systemctl enable shared-folders.service || {
-        echo "Error: Failed to enable shared-folders.service."
+        echo -e "\033[1mΣφάλμα: Αποτυχία ενεργοποίησης shared-folders.service.\033[1m"
         exit 1
     }
-    systemctl enable user-defaults.service || {
-        echo "Error: Failed to enable user-defaults.service."
-        exit 1
-    }
-    systemctl start shared-folders.service && echo "shared-folders.service enabled successfully." || {
-        echo "Error: Failed to start shared-folders.service."
+    systemctl start shared-folders.service && echo -e "\033[1mshared-folders.service ενεργοποιήθηκε επιτυχώς.\033[1m" || {
+        echo -e "\033[1mΣφάλμα: Αποτυχία ενεργοποίησης shared-folders.service.\033[1m"
         exit 1
     }
 
     systemctl daemon-reload
     
-    systemctl start user-defaults.service && echo "user-defaults.service started successfully." || {
-        echo "Error: Failed to start user-defaults.service."
-        exit 1
-    }
-    
 }
 create_public_folder() {
-    echo "Creating the public folder..."
+    echo -e "\033[1mΔιμηουργία Δημόσιου φακέλου...\033[1m"
     test -d "$PUBLIC_DIR" && return 0
     mkdir -p "$PUBLIC_DIR"
     if [ $? -ne 0 ]; then
-        echo "Error: Failed to create $PUBLIC_DIR."
+        echo -e "\033[1mΣφάλμα: Αποτυχία δημιουργίας $PUBLIC_DIR.\033[1m"
         exit 1
     fi
     chmod 0777 "$PUBLIC_DIR"
 }
 install_files() {
-    echo "Moving files to their destinations..."
+    echo -e "\033[1mΜετακίνηση αρχείων στον προορισμό τους...\033[1m"
     mkdir -p "$DEST_ETC" "$DEST_LIB" "$DEST_SHARE" "$DEST_SBIN" "$DEST_ROOT" "$DEST_CONFIGS" "$DEST_UI" "$DEST_BINS"
     if [ ! -f "$DEST_ETC/default/shared-folders" ]; then
-        install -o root -g root -m 0644 "$PROJECT_ETC/default/shared-folders.default" "$DEST_ETC/default/shared-folders" || { echo "$ERROR_MOVE_FILES"; exit 1; }
+        install -o root -g root -m 0644 "$PROJECT_ETC/default/shared-folders.default" "$DEST_ETC/default/shared-folders" || { echo -e "$ERROR_MOVE_FILES"; exit 1; }
     fi
     for file in "$PROJECT_ETC"/*; do
          if [[ "$DEST_ETC" != "$DEST_ROOT" ]] && [[ "$DEST_ETC" != "$DEST_SBIN" ]] && [[ "$file" != "$PROJECT_ETC/default/shared-folders.default" ]]; then
             backup_file "$DEST_ETC" "$file"
         fi
-        install_path "$file" "$DEST_ETC" || { echo "$ERROR_MOVE_FILES"; exit 1; }
+        install_path "$file" "$DEST_ETC" || { echo -e "$ERROR_MOVE_FILES"; exit 1; }
     done
     for file in "$PROJECT_LIB"/*; do
         if [[ "$DEST_LIB" != "$DEST_ROOT" ]] && [[ "$DEST_LIB" != "$DEST_SBIN" ]]; then
             backup_file "$DEST_LIB" "$file"
         fi
-        install_path "$file" "$DEST_LIB" || { echo "$ERROR_MOVE_FILES"; exit 1; }
+        install_path "$file" "$DEST_LIB" || { echo -e "$ERROR_MOVE_FILES"; exit 1; }
     done
     for file in "$PROJECT_SHARE"/*; do
         if [[ "$DEST_SHARE" != "$DEST_ROOT" ]] && [[ "$DEST_SHARE" != "$DEST_SBIN" ]]; then
             backup_file "$DEST_SHARE" "$file"
         fi
-        install_path "$file" "$DEST_SHARE" || { echo "$ERROR_MOVE_FILES"; exit 1; }
+        install_path "$file" "$DEST_SHARE" || { echo -e "$ERROR_MOVE_FILES"; exit 1; }
     done
     for file in "$PROJECT_ROOT"/*; do
         if [[ ! "$file" == "$PROJECT_ROOT/configs" ]] && [[ ! "$file" == "$PROJECT_ROOT/ui" ]] && [[ ! "$file" == "$PROJECT_ROOT/scripts" ]]; then 
-           install_path "$file" "$DEST_ROOT" || { echo "$ERROR_MOVE_FILES"; exit 1; }
+           install_path "$file" "$DEST_ROOT" || { echo -e "$ERROR_MOVE_FILES"; exit 1; }
         fi
     done
     for file in "$PROJECT_SBIN"/*; do
-       install_path "$file" "$DEST_SBIN" || { echo "$ERROR_MOVE_FILES"; exit 1; }
+       install_path "$file" "$DEST_SBIN" || { echo -e "$ERROR_MOVE_FILES"; exit 1; }
     done
     for file in "$PROJECT_CONFIGS"/*; do
-        install_path "$file" "$DEST_CONFIGS" || { echo "$ERROR_MOVE_FILES"; exit 1; }
+        install_path "$file" "$DEST_CONFIGS" || { echo -e "$ERROR_MOVE_FILES"; exit 1; }
     done
     for file in "$PROJECT_UI"/*; do
-        install_path "$file" "$DEST_UI" || { echo "$ERROR_MOVE_FILES"; exit 1; }
+        install_path "$file" "$DEST_UI" || { echo -e "$ERROR_MOVE_FILES"; exit 1; }
     done
     for file in "$PROJECT_BINS"/*; do
-        install_path "$file" "$DEST_BINS" || { echo "$ERROR_MOVE_FILES"; exit 1; }
+        install_path "$file" "$DEST_BINS" || { echo -e "$ERROR_MOVE_FILES"; exit 1; }
     done
     install -o root -g root -m 0644 "etc/systemd/system/shared-folders.service" "$DEST_ETC/systemd/system/shared-folders.service" || {
-        echo "Failed to create /etc/systemd/system/shared-folders.service"
-        exit 1
-    }
-    sed -i 's|Type=simple|Type=forking|g' "$DEST_ETC/systemd/system/user-defaults.service" 
-    sed -i 's|After=graphical-session.target|After=shared-folders.service|g' "$DEST_ETC/systemd/system/user-defaults.service"
-    install -o root -g root -m 0644 "etc/systemd/system/user-defaults.service" "$DEST_ETC/systemd/system/user-defaults.service" || {
-        echo "Failed to create /etc/systemd/system/user-defaults.service"
+        echo -e "\033[1mΑποτυχία δημιουργίας /etc/systemd/system/shared-folders.service\033[1m"
         exit 1
     }
     install -o root -g root -m 0755 "sbin/shared-folders" "$DEST_SBIN/shared-folders" || {
-        echo "Failed to create /usr/sbin/shared-folders."
+        echo -e "Αποτυχία δημιουργίας /usr/sbin/shared-folders."
         exit 1
     }
-    install -o root -g root -m 0755 "sbin/user-defaults" "$DEST_SBIN/" || {
-        echo "Failed to create /usr/sbin/user-defaults."
-        exit 1
-    }
-    echo "Files moved successfully."
+    echo -e "\033[1mΤα αχρεία μετακινήθηκαν επιτυχώς.\033[1m"
 } 
 revert_files() {
-    echo "Reverting file changes..."
+    echo -e "\033[1mΕπαναφορά αρχείων...\033[1m"
     for file in "$PROJECT_ETC"/*; do
          if [[ "$DEST_ETC" != "$DEST_ROOT" ]] && [[ "$DEST_ETC" != "$DEST_SBIN" ]]; then
             revert_file "$DEST_ETC" "$file"
@@ -278,38 +259,35 @@ revert_files() {
     done
     rm -rf "$PUBLIC_DIR"
     revert_file "$DEST_ETC/systemd/system" "etc/systemd/system/shared-folders.service"
-    rm -rf "$PUBLIC_DIR"
-    revert_file "$DEST_ETC/systemd/system" "etc/systemd/system/user-defaults.service"
     revert_file "$DEST_SBIN" "sbin/shared-folders"
-    echo "File changes reverted successfully."
-    revert_file "$DEST_SBIN" "sbin/user-defaults"
-    echo "File changes reverted successfully."
+    echo -e "\033[1mΤα αρχεία επαναφέρθηκαν επιτυχώς.\033[1m"
+
 }
 install_sch() {
-    echo "Installing sch-scripts..."
+    echo -e "\033[1mΕγκατάσταση των sch-scripts...\033[1m"
 
     install_dependencies
-    echo "Dependencies installed successfully."
+    echo -e "\033[1mΟι εξαρτήσεις εγκαταστήθηκαν απιτυχώς.\033[1m"
     install_files
     create_public_folder
     configure_teachers
     start_services
-    echo "Installation of sch-scripts completed successfully!"
+    echo -e "\033[1mΗ εγκατάσταση των sch-scripts ολοκληρώθηκε με επιτυχία!\033[1m"
 }
 
 remove_sch() {
-    echo "Removing sch-scripts..."
+    echo -e "\033[1mΑφαίρεση των sch-scripts...\033[1m"
     remove_dependencies
-    echo "Dependencies removed successfully."
+    echo -e "\033[1mΟι εξαρτήσεις αφαιρέθηκαν επιτυχώς.\033[1m"
     revert_files
-    echo "Revert of sch-scripts completed successfully!"
+    echo -e "\033[1mΗ επεναφορά και αφαίρεση των sch-scripts ολοκληρώθηκε επιτυχώς!\033[1m"
 }
 
 main() {
     if [[ "$1" == "-u" ]]; then
         REVERT=true
     fi
-    echo "Value of REVERT inside main: $REVERT"
+    echo -e "\033[1mΤιμή του VALUE είναι: $REVERT\033[1m"
     if [[ $REVERT == false ]]; then
         install_sch "$@"
     else
