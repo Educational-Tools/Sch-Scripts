734d887a28706122fdc5c27f3fddcfa54c127481
Dimitrys <demetresmeliates@gmail.com>
2025-03-24 21:58:14 +0200

    modified:   debian/rules 	modified:   debian/sch-scripts.postinst 	deleted:    debian/user-defaults.service 	deleted:    share/sch-scripts/scripts/user-defaults.sh

--- share/sch-scripts/scripts/user-defaults.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# User Defaults
-
-gsettings set org.cinnamon desktop-effects-workspace false
-gsettings set org.cinnamon.settings-deamon.peripherals.keyboard input-sources-switcher 'alt-shift-l'
