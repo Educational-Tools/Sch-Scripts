f9a2f060f607545dff9c80626b85d096f6883e6f
Dimitrys <demetresmeliates@gmail.com>
2025-03-25 19:39:41 +0200

    modified:   debian/sch-scripts.gsettings-override 	modified:   debian/sch-scripts.postinst 	modified:   share/sch-scripts/conf/firefox.js

--- share/sch-scripts/conf/firefox.js
+++ share/sch-scripts/conf/firefox.js
@@ -1,8 +1,22 @@
 // Disable firefox cache
-pref("browser.cache.disk.enable",false);
+pref("browser.cache.disk.enable", false);
 
 // Disable internal PDF viewer
-pref("pdfjs.disabled",true);
+pref("pdfjs.disabled", true);
 
 // Enable flash on file:// URLs
-pref("plugins.http_https_only",false);
+pref("plugins.http_https_only", false);
+
+// Install uBlock Origin
+pref("extensions.autoDisableScopes", 10);
+pref("extensions.blocklist.enabled", false);
+pref("extensions.install.requireBuiltInCerts", false);
+pref("extensions.install.allowInstallationWithoutUserInteraction", true);
+pref("extensions.install.allowDowngrades", true);
+pref("extensions.update.enabled", true);
+pref("extensions.update.autoUpdateDefault", true);
+pref("xpinstall.signatures.required", false);
+
+// uBlock Origin extension ID
+pref("extensions.install.url", "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/addon-607465-latest.xpi");
+pref("extensions.install.id", "uBlock0@raymondhill.net");
