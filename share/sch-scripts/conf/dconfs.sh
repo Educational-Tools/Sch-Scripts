9a505655d6c032a5a7ab4fb5173331e380bdc6ef
Dimitrys <demetresmeliates@gmail.com>
2025-03-28 19:32:37 +0200

    modified:   share/sch-scripts/conf/dconfs.sh

--- share/sch-scripts/conf/dconfs.sh
+++ share/sch-scripts/conf/dconfs.sh
@@ -1,9 +1,14 @@
-# Apply dconf settings for the current user
-profile_file="$HOME/.profile"
-if [ -f "$profile_file" ] && ! grep -q "dconf" "$profile_file"; then
-    {
-        echo "dconf write /org/gnome/libgnomekbd/keyboard/layouts \"['gr', 'us']\""
-        echo "dconf write /org/gnome/libgnomekbd/keyboard/options \"['grp\tgrp:alt_shift_toggle']\""
-        echo "dconf update"
-    } >> "$profile_file"
-fi
+server_hostname=$(hostname)
+
+for user in /home/*; do
+    if [ -d "$user" ] && [ "$(basename "$user")" != "Shared" ]; then
+        profile_file="$user/.profile"
+        if [ -f "$profile_file" ] && ! grep -q "dconf" "$profile_file"; then
+            {
+                echo "dconf write org.gnome.libgnomekbd.keyboard layouts \"['gr', 'us']\""
+                echo "dconf write org.gnome.libgnomekbd.keyboard options \"['grp\tgrp:alt_shift_toggle']\""
+                echo "gsettings set org.gnome.desktop.background picture-uri \\\"file:///usr/share/backgrounds/sch-walls/${server_hostname}.png\\\""
+            } >> "$profile_file"
+        fi
+    fi
+done
