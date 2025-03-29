b330a943748c5b7597d20b105225f564c5249c05
Dimitrys <demetresmeliates@gmail.com>
2025-03-29 09:57:21 +0200

    modified:   share/sch-scripts/scripts/user_defaults.sh

--- share/sch-scripts/scripts/user_defaults.sh
+++ share/sch-scripts/scripts/user_defaults.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # This script is part of sch-scripts, https://sch-scripts.gitlab.io
 # Copyright 2009-2022 the sch-scripts team, see AUTHORS
 # SPDX-License-Identifier: GPL-3.0-or-later
@@ -9,10 +10,15 @@ if [ "$(id -un)" != "administrator" ]; then
     exit 1
 fi
 
+# Debug: Print all arguments received
+echo "Arguments received: $@"
+
 # Function to set the wallpaper
 set_wallpaper() {
     hostname=$(hostname)
     wallpaper_path="/usr/share/backgrounds/sch-walls/${hostname}.png"
+
+    echo "Setting wallpaper for all users to: ${wallpaper_path}"
 
     for user_home in /home/*; do
         if [ -d "$user_home" ] && [ "$(basename "$user_home")" != "Shared" ]; then
