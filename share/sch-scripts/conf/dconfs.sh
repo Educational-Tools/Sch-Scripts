b504549552f29c7b569e1d41ffedb428da3ff459
Dimitrys <demetresmeliates@gmail.com>
2025-03-29 13:37:44 +0200

    modified:   share/sch-scripts/conf/dconfs.sh

--- share/sch-scripts/conf/dconfs.sh
+++ share/sch-scripts/conf/dconfs.sh
@@ -1,5 +1,3 @@
-profile = $(whoami)/.profile
-
-echo "dconf write org.gnome.libgnomekbd.keyboard layouts \"['gr', 'us']\"" >> profile
-echo "dconf write org.gnome.libgnomekbd.keyboard options \"['grp\tgrp:alt_shift_toggle']\"" >> profile
-echo "dconf update" >> profile
+echo "dconf write org.gnome.libgnomekbd.keyboard layouts \"['gr', 'us']\"" >> .profile
+echo "dconf write org.gnome.libgnomekbd.keyboard options \"['grp\tgrp:alt_shift_toggle']\"" >> .profile
+echo "dconf update" >> .profile
