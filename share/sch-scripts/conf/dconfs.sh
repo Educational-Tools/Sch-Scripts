14c667737f6b43388223e548a4f50dd41403320e
Dimitrys <demetresmeliates@gmail.com>
2025-03-29 18:01:05 +0200

    modified:   share/sch-scripts/conf/dconfs.sh

--- share/sch-scripts/conf/dconfs.sh
+++ share/sch-scripts/conf/dconfs.sh
@@ -1,6 +1,3 @@
-if [ .profile == "dconf" ]; do
-    echo "dconf write /org/gnome/libgnomekbd/keyboard/layouts \"['gr', 'us']\"" >> .profile
-    echo "dconf write /org/gnome/libgnomekbd/keyboard/options \"['grp\tgrp:alt_shift_toggle']\"" >> .profile
-    echo "gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/sch-walls/$(hostname).png'" >> .profile
-    echo "dconf update" >> .profile
-fi
+dconf write /org/gnome/libgnomekbd/keyboard/layouts "['gr', 'us']"
+dconf write /org/gnome/libgnomekbd/keyboard/options "['grp\tgrp:alt_shift_toggle']"
+dconf update
