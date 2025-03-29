ea4226540021b312b6681b3247ba093c8cd98094
Dimitrys <demetresmeliates@gmail.com>
2025-03-29 09:03:49 +0200

    modified:   share/sch-scripts/user_defaults.py

--- share/sch-scripts/user_defaults.py
+++ share/sch-scripts/user_defaults.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# This file is part of sch-scripts, https://sch-scripts.gitlab.io
+# Copyright 2009-2022 the sch-scripts team, see AUTHORS
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Sch-scripts user defaults.
+"""
 import os
 import subprocess
 import socket
